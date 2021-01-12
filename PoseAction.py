01998ace867f8370e77bfa5c35e1a24f4d6b7df9
kiba-shin <sirugisa@gmail.com>
2021-01-12 11:17:54 +0900

    学習モデル変更 ポーズ微調整

--- PoseAction.py
+++ PoseAction.py
@@ -23,22 +23,27 @@ def action(sign_id,x,y,countpose):
 
     if(sign_id==2):
         #gunの時の処理
-        if(countpose[2]<=3):
-            countpose[2] += 1
-        if(countpose[2]==3):
-            autopy.mouse.click(autopy.mouse.Button.LEFT)
-    if(sign_id==3):
-        if(countpose[3]<3):
-            countpose[3] += 1
-        if(countpose[3]==3):
-            autopy.mouse.toggle(autopy.mouse.Button.LEFT,True)
-            pointermove(x,y)
-
-    if(sign_id==4):
         if(countpose[4]<=3):
             countpose[4] += 1
         if(countpose[4]==3):
             autopy.mouse.click(autopy.mouse.Button.RIGHT)
+    if(sign_id==3):
+        #peaceの時
+        if(countpose[3]<4):
+            countpose[3] += 1
+        if(countpose[3]==3):
+            autopy.mouse.toggle(autopy.mouse.Button.LEFT,True)
+            pointermove(x,y)
+        if(countpose[3]==4):
+            pointermove(x,y)
+
+    if(sign_id==4):
+        #rockの時
+        if(countpose[2]<=3):
+            countpose[2] += 1
+        if(countpose[2]==3):
+            autopy.mouse.click(autopy.mouse.Button.LEFT)
+
 
     # if(sign_id==5):
         #OKの
