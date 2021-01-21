73994f6132201075ff981cdd001bd42f8534beb6
AdiosSora <adiossora1202@gmail.com>
2021-01-21 15:02:43 +0900

    ライブラリインストール用のファイルとバッチを追加

---
+++ requirements.bat
@@ -0,0 +1,6 @@
+py -m pip install -U archive/mediapipe-0.8.2-cp38-cp38-win_amd64.whl
+py -m pip install -U archive/tensorflow-2.4.0-cp38-cp38-win_amd64.whl
+py -m pip install -U archive/pywin32-300-cp38-cp38-win_amd64.whl
+py -m pip install -U archive/PyAutoGUI-0.9.52.tar.gz
+py -m pip install -U archive/Eel-0.14.0.tar.gz
+py -m pip install -U archive/autopy-4.0.0-cp38-cp38-win_amd64.whl
