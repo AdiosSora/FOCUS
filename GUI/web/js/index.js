ab3edce2b2da8a1920bf947f9b432f971f44f5dc
AdiosSora <adiossora1202@gmail.com>
2021-01-26 10:37:01 +0900

    使わなくなった関数が干渉していたので削除

--- GUI/web/js/index.js
+++ GUI/web/js/index.js
@@ -93,13 +93,3 @@ eel.expose(windowclose)
 function windowclose(){
   window.close();
 }
-
-eel.expose(set_posegauge);
-function set_posegauge(cnt_pose, name_pose){
-
-  /*７割越えのポーズのゲージのみを取得したい場合はこれ*/
-  var target = document.getElementById("poseGuage");
-
-  target.innerHTML = cnt_pose + '回，' + name_pose + '<meter max= "100" min= "0" low= "20" high= "80" optimum= "90" value= ' + cnt_pose +'></meter>';
-
-}
