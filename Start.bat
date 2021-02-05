8770b76cb261e66dc8902b564ac4614b9963edcc
AdiosSora <adiossora1202@gmail.com>
2021-02-05 14:36:10 +0900

    Commit

--- Start.bat
+++ Start.bat
@@ -4,4 +4,11 @@ if not "%~0"=="%~dp0.\%~nx0" (
      start /min cmd /c,"%~dp0.\%~nx0" %*
      exit
 )
+bin\python.exe -m pip install tensorflow==2.4.0
+bin\python.exe -m pip install mediapipe==0.8.2
+bin\python.exe -m pip install eel==0.14.0
+bin\python.exe -m pip install pywin32==300
+bin\python.exe -m pip install autopy==4.0.0
+bin\python.exe -m pip install PyAutoGUI==0.9.52
 bin\python.exe Main.py
+pause
---
+++ uninstall.bat
@@ -0,0 +1,6 @@
+bin\python.exe -m pip uninstall -y tensorflow==2.4.0
+bin\python.exe -m pip uninstall -y mediapipe==0.8.2
+bin\python.exe -m pip uninstall -y eel==0.14.0
+bin\python.exe -m pip uninstall -y pywin32==300
+bin\python.exe -m pip uninstall -y autopy==4.0.0
+bin\python.exe -m pip uninstall -y PyAutoGUI==0.9.52
