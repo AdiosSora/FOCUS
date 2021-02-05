5ceaede1f94d699bd4df06bfa38c21fb12675f8e
AdiosSora <adiossora1202@gmail.com>
2021-02-05 15:35:17 +0900

    バッチファイル調整

--- Start.bat
+++ Start.bat
@@ -1,9 +1,3 @@
 @echo off
-bin\python.exe -m pip install tensorflow==2.4.0
-bin\python.exe -m pip install mediapipe==0.8.2
-bin\python.exe -m pip install eel==0.14.0
-bin\python.exe -m pip install pywin32==300
-bin\python.exe -m pip install autopy==4.0.0
-bin\python.exe -m pip install PyAutoGUI==0.9.52
-bin\python.exe Main.py
-pause
+
+start /min %~dp0utils\Start.bat %*
